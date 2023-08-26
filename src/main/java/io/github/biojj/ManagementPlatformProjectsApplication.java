681ccd4d2d7406273c379f73ade782ee0af233a2
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-25 22:41:44 -0400

    feat: ✨ init

---
+++ src/main/java/io/github/biojj/ManagementPlatformProjectsApplication.java
@@ -0,0 +1,13 @@
+package io.github.biojj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPlatformProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPlatformProjectsApplication.class, args);
+	}
+
+}
